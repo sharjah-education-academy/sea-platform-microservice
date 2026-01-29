ea27c5e183fb3fb511505c4070c610d3bad27729
Mohammed Masri <mqalmasri@sea.ac.ae>
2026-01-29 13:40:27 +0400

    send an OTP email for the forget password

--- src/modules/otp.module.ts
+++ src/modules/otp.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { otpProviders } from 'src/models/otp/otp.provider';
 import { OTPService } from 'src/models/otp/otp.service';
+import { QueueModule } from 'src/queue/queue.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, QueueModule],
   providers: [OTPService, ...otpProviders],
   exports: [OTPService],
 })
